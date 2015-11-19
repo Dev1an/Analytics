acf3d24870b4239557c776b964ed5f468f004962
Devian <info@devian.tk>
2015-11-19 10:35:45 +0100

    bump to 0.0.11

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
+## v0.0.11
+
+* Rename History.md to HISTORY.md
+
 ## v.0.0.10
 
 * Add geospatial data analysis (based on IP address).
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'devian:analytics',
-	version: '0.0.10',
+	version: '0.0.11',
 	// Brief, one-line summary of the package.
 	summary: 'Simple real-time analytics for free',
 	// URL to the Git repository containing the source code for this package.
